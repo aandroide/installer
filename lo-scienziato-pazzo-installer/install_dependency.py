ec4a41368ea8b74b6e3f94d6ce4268cf05f9b75b
Mahmoud Atef <mahmoudatef.coder@gmail.com>
2023-11-21 20:19:30 +0200

    update to work in resperi-pi

--- lo-scienziato-pazzo-installer/install_dependency.py
+++ lo-scienziato-pazzo-installer/install_dependency.py
@@ -2,6 +2,7 @@
 import io
 import platform
 import xbmc, os, shutil, json
+import subprocess
 # functions that on kodi 19 moved to xbmcvfs
 try:
     import xbmcvfs
@@ -21,15 +22,32 @@ except ImportError:
 
 def run():
     #get platform
-    platform_name = str(platform.system())+"-"+str(platform.machine())
-    platform_name=platform_name.lower()
-    logger.info("platform_name : ",platform_name)
-    
+    # platform_name = str(platform.system())+"-"+str(platform.machine())
+    # platform_name=platform_name.lower()
+    # logger.info("platform_name : ",platform_name)
+    if xbmc.getCondVisibility('system.platform.linux') and not xbmc.getCondVisibility('system.platform.android'):
+        dp = platformtools.dialog_progress_bg(config.get_localized_string(20000),config.get_localized_string(90050) )
+        dp.update(0)
 
-    xbmc.executebuiltin("InstallAddon({})".format("pvr.iptvsimple"))
-    xbmc.sleep(10000)
-    if xbmc.getCondVisibility('System.HasAddon({})'.format("pvr.iptvsimple")):
-        xbmc.executebuiltin("RunAddon(plugin.video.lo-scienziato-pazzo)")
+        result =subprocess.run(["sudo","apt-get","install","kodi-pvr-iptvsimple","-y"])
+        dp.update(95)
+        if result.returncode==0:
+            logger.info("installed depenceny success")
+            xbmc.sleep(5000)
+            xbmc.executebuiltin("UpdateLocalAddons")
+            dp.update(100)
+            xbmc.executebuiltin("RunAddon(plugin.video.lo-scienziato-pazzo)")
+        else:
+            logger.info("Error in installing")
+            platformtools.dialog_ok(config.get_localized_string(20000), config.get_localized_string(90051))
+            return
+        dp.close()
+    else:
+        xbmc.executebuiltin("InstallAddon({})".format("pvr.iptvsimple"))
+        xbmc.sleep(10000)
+        xbmc.executebuiltin("UpdateLocalAddons")
+        if xbmc.getCondVisibility('System.HasAddon({})'.format("pvr.iptvsimple")):
+            xbmc.executebuiltin("RunAddon(plugin.video.lo-scienziato-pazzo)")
     # xbmc.executebuiltin("RunScript(special://home/addons/plugin.video.lo-scienziato-pazzo/default.py)")
 
 if __name__ == "__main__":
