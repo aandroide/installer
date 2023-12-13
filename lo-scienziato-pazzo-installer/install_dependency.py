ed7eb9e4401c4862f8c41981ed7562a8123b2392
aandroide <14141574+aandroide@users.noreply.github.com>
2023-12-13 21:39:33 +0100

    Update install_dependency.py

--- lo-scienziato-pazzo-installer/install_dependency.py
+++ lo-scienziato-pazzo-installer/install_dependency.py
@@ -141,6 +141,7 @@ def install_dep_in_linux():
         
         dp.close()               
     except Exception as ex:
+        logger.exception("Error in installing")
         logger.info("Error in installing")
         platformtools.dialog_ok(config.get_localized_string(20000), config.get_localized_string(90051))
         dp.close()
