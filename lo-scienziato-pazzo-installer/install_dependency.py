44c035f51f4d416e598783f13f5d6a3bf7c728e1
aandroide <14141574+aandroide@users.noreply.github.com>
2023-12-18 19:21:48 +0100

    Update install_dependency.py

--- lo-scienziato-pazzo-installer/install_dependency.py
+++ lo-scienziato-pazzo-installer/install_dependency.py
@@ -84,11 +84,13 @@ def get_platform():
         print("Detected OS: Linux")
         if "Ubuntu" in os.uname()[3]:
           return "Ubuntu"
-        #elif "arm" in platform.machine().lower():
-        #    try:
-        #        with open ('/sys/firmware/devicetree/base/model','r') as model:
-        #            if 'raspberry pi' in model.read().lower():
-        #              return "Raspberry pi"
+        elif "arm" in platform.machine().lower():
+            try:
+                with open ('/sys/firmware/devicetree/base/model','r') as model:
+                    if 'raspberry pi' in model.read().lower():
+                      return "Raspberry pi"
+        except:
+            pass
     else:
         return "Unknown"
         
