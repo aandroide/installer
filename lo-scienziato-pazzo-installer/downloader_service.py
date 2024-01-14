deb11bbb04a7f0ee6854c8ef1e8722dbad44c2ff
aandroide <14141574+aandroide@users.noreply.github.com>
2024-01-14 11:00:22 +0100

    Update downloader_service.py

--- lo-scienziato-pazzo-installer/downloader_service.py
+++ lo-scienziato-pazzo-installer/downloader_service.py
@@ -72,9 +72,9 @@ def chooseBranch():
         return False
     branches = json.loads(branches)
     chDesc = [config.get_localized_string(80034), config.get_localized_string(80035)]
-    chDesc.extend([b['name'] for b in branches if b['name'] not in ['stable', 'master']])
-    chName = ['stable', 'master']
-    chName.extend([b['name'] for b in branches if b['name'] not in ['stable', 'master']])
+    chDesc.extend([b['name'] for b in branches if b['name'] not in ['stable']])
+    chName = ['stable']
+    chName.extend([b['name'] for b in branches if b['name'] not in ['stable']])
     sel = platformtools.dialog_select(config.get_localized_string(80033), chDesc)
     if sel == -1:
         return False
